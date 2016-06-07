971577a3f3d6275f5e91d499c2a2ec3a46f6bf1d
y_uuki <yuki.tsubo@gmail.com>
2016-06-07 12:11:24 +0900

    Add --version flag

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 const version = "0.1.0"
 
 type opts struct {
-	Host    string `short:"H" long:"host" required:"true" value-name:"hostname" description:"ikachan hostname"`
+	Host    string `short:"H" long:"host" value-name:"hostname" description:"ikachan hostname"`
 	Port    string `short:"p" long:"port" value-name:"port" default:"4979" description:"ikachan port"`
-	Channel string `short:"c" long:"channel" required:"true" value-name:"'#channel'" description:"destination channel"`
+	Channel string `short:"c" long:"channel" value-name:"'#channel'" description:"destination channel"`
 	MsgType string `short:"t" long:"type" value-name:"msgtype" default:"notice" description:"message type notice/privmsg)"`
 	Stream  bool   `short:"s" long:"stream" description:"messages to Ikachan continuously"`
+	Version bool   `short:"V" long:"version" description:"show version"`
 }
 
 func readIn(lines chan string) {
@@ -27,6 +28,10 @@ func readIn(lines chan string) {
 	close(lines)
 }
 
+func printVersion() {
+	fmt.Printf("ikachancat version v%s\n", version)
+}
+
 func main() {
 	o := &opts{}
 	p := flags.NewParser(o, flags.Default)
@@ -35,6 +40,22 @@ func main() {
 	if err != nil {
 		p.WriteHelp(os.Stderr)
 		os.Exit(-1)
+	}
+
+	if o.Version {
+		printVersion()
+		os.Exit(0)
+	}
+
+	if o.Host == "" {
+		fmt.Fprintf(os.Stderr, "--host required\n")
+		p.WriteHelp(os.Stderr)
+		os.Exit(1)
+	}
+	if o.Channel == "" {
+		fmt.Fprintf(os.Stderr, "--channel required\n")
+		p.WriteHelp(os.Stderr)
+		os.Exit(1)
 	}
 
 	lines := make(chan string)
